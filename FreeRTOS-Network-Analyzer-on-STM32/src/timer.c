09bb18fe0b7e47a0753ef0eb961a4c9739888185
jmfriedt <jmfriedt>
2018-09-02 11:14:40 +0200

    reset timer

--- FreeRTOS-Network-Analyzer-on-STM32/src/timer.c
+++ FreeRTOS-Network-Analyzer-on-STM32/src/timer.c
@@ -1,10 +1,11 @@
 #include <libopencm3/stm32/timer.h>
-
+#include <libopencm3/stm32/rcc.h>
 #include "timer.h"
 
 void init_Timer(uint8_t frequencyMHz)
 {
-	timer_reset(TIM1);
+	// timer_reset(TIM1); // old libopencm3
+        rcc_periph_reset_pulse(RST_TIM1);
 	timer_set_mode(TIM1, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
 	timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM2);
 	timer_enable_oc_output(TIM1, TIM_OC1);
@@ -19,3 +20,6 @@ void setTimer(uint8_t frequencyMHz)
 {
 	timer_set_prescaler(TIM1, (70/frequencyMHz)-1);
 }
+
+
+
