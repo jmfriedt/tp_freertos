2cf2090444c974fb95785334838aacca8879fe76
jmfriedt <jean-michel.friedt@femto-st.fr>
2019-03-09 14:48:41 +0100

    Only handle LEDs *after* init clocks

--- 5philosophe/src/main.c
+++ 5philosophe/src/main.c
@@ -1,8 +1,3 @@
-// 170308: ajout des conditions d'echec
-// * on ne prend la 2eme baguette que si la 1ere a ete obtenue
-// * on re-essaie tant qu'on n'a pas mange'
-// * on n'a le droit de manger que si on a 2 baguettes (correction)
-// TODO : etudier l'impact des diverses latences
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
@@ -15,25 +10,19 @@ int mange[NB_PHILO];
 
 void func(void* p)
 { int numero= *(int*) p;
-  mange[numero]=0;
   while (mange[numero]!=1)
     {uart_putc('a'+numero);vTaskDelay(500 / portTICK_RATE_MS);
-     if (xSemaphoreTake(xMutex[numero],500/portTICK_RATE_MS)!=pdFALSE)  // prend 1re  baguette
-       {uart_putc('v'+numero); //vTaskDelay(500 / portTICK_RATE_MS);   // prend 2eme baguette si 1ere ok
-        if (xSemaphoreTake(xMutex[(numero+1)%NB_PHILO],500/portTICK_RATE_MS)==pdFALSE)
-           {uart_putc('m'+numero);
-            // uart_puts("ng\r\n\0");
-            xSemaphoreGive( xMutex[numero] );                          // sinon repose la 1ere baguette
-           }
-        else
-           {                                                         
-            uart_putc('A'+numero);vTaskDelay(500 / portTICK_RATE_MS);  // 2 baguettes = on mange
-            xSemaphoreGive( xMutex[numero] );                          // on repose tout
-            xSemaphoreGive( xMutex[(numero+1)%NB_PHILO] );
-            uart_putc('0'+numero);
-            mange[numero]=1;                                           // fini
-           }
-       } // on a rate' la 1ere baguette
+     if (xSemaphoreTake(xMutex[numero],500/portTICK_RATE_MS)==pdFALSE)
+        {uart_putc('u'+numero);
+         uart_puts("ng\r\n\0");
+         xSemaphoreGive( xMutex[numero] );
+        }
+     xSemaphoreTake( xMutex[(numero+1)%NB_PHILO], portMAX_DELAY );
+     uart_putc('A'+numero);vTaskDelay(500 / portTICK_RATE_MS);
+     xSemaphoreGive( xMutex[numero] );
+     xSemaphoreGive( xMutex[(numero+1)%NB_PHILO] );
+     uart_putc('0'+numero);
+     mange[numero]=1;
     }
   while (1) { vTaskDelay(100 / portTICK_RATE_MS); }; // on n'a jamais le droit de finir toutes les taches
 }
