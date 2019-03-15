e8dd5d33ec7f23c4036acacfeb9e6adc3d5f5100
jmfriedt <jean-michel.friedt@femto-st.fr>
2019-03-15 17:36:02 +0100

    ADC

--- 6adc/src/FreeRTOSConfig.h
+++ 6adc/src/FreeRTOSConfig.h
@@ -73,13 +73,15 @@
 #define configUSE_TICK_HOOK			0
 #define configCPU_CLOCK_HZ			( ( unsigned long ) 72000000 )
 #define configTICK_RATE_HZ			( ( portTickType ) 1000 )
-#define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 5 )
+#define configMAX_PRIORITIES		( 5 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 10 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 5* 1024 ) )
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 4* 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 16 )
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
+
+#define configUSE_MUTEXES 1  // jmf POUR MUTEX
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0
@@ -95,8 +97,6 @@ to exclude the API function. */
 #define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
 #define INCLUDE_vTaskDelay				1
-
-#define configUSE_MUTEXES 1
 
 /* This is the raw value as per the Cortex-M3 NVIC.  Values can be 255
 (lowest) to 0 (1?) (highest). */
