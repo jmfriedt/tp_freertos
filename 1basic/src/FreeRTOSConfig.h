52845a7e67ebd0c2ca913790fd20a8af653fa087
Jean-Michel Friedt <friedtj@free.fr>
2023-03-17 10:29:42 +0100

    AVR support

--- 1basic/src/FreeRTOSConfig.h
+++ 1basic/src/FreeRTOSConfig.h
@@ -66,17 +66,24 @@
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
-#define STACK_BYTES(x)                                  ((x)/4) /* freeRTOS allocates 4-times the size given due to 32-bit ARM */
+#define STACK_BYTES(x)                  ((x)/4) /* freeRTOS allocates 4-times the size given due to 32-bit ARM */
 
 #define configUSE_PREEMPTION		1
-#define configUSE_IDLE_HOOK			0
-#define configUSE_TICK_HOOK			0
-#define configCPU_CLOCK_HZ			( ( unsigned long ) 72000000 )
-#define configTICK_RATE_HZ			( ( portTickType ) 1000 )
+#define configUSE_IDLE_HOOK		0
+#define configUSE_TICK_HOOK		0
+#if defined (__AVR_ATmega32U4__)
+#warning compiling for Atmega32U4
+#define configCPU_CLOCK_HZ		( ( unsigned long ) 16000000 )
+#define configTICK_RATE_HZ  ( (TickType_t)( (uint32_t)128000 >> (portUSE_WDTO + 11) ) )  // 2^11 = 2048 WDT scaler for 128kHz Timer
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 2* 1024 ) )
+#else
+#define configCPU_CLOCK_HZ		( ( unsigned long ) 72000000 )
+#define configTICK_RATE_HZ		( ( portTickType ) 1000 )
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 5* 1024 ) )
+#endif
 // #define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 5 ) // JMF compatible v9
 #define configMAX_PRIORITIES		( 5 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 64 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 5* 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 16 )
 
 #define configUSE_16_BIT_TICKS		0
@@ -91,19 +98,18 @@
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet		0
-#define INCLUDE_uxTaskPriorityGet		0
-#define INCLUDE_vTaskDelete				0
+#define INCLUDE_vTaskPrioritySet	0
+#define INCLUDE_uxTaskPriorityGet	0
+#define INCLUDE_vTaskDelete		0
 #define INCLUDE_vTaskCleanUpResources	0
-#define INCLUDE_vTaskSuspend			1
-#define INCLUDE_vTaskDelayUntil			1
-#define INCLUDE_vTaskDelay				1
+#define INCLUDE_vTaskSuspend		1
+#define INCLUDE_vTaskDelayUntil		1
+#define INCLUDE_vTaskDelay		1
 
 /* This is the raw value as per the Cortex-M3 NVIC.  Values can be 255
 (lowest) to 0 (1?) (highest). */
-#define configKERNEL_INTERRUPT_PRIORITY 		255
+#define configKERNEL_INTERRUPT_PRIORITY		255
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY 	191 /* equivalent to 0xb0, or priority 11. */
-
 
 /* This is the value being used as per the ST library which permits 16
 priority values, 0 to 15.  This must correspond to the
@@ -112,5 +118,3 @@ NVIC value of 255. */
 #define configLIBRARY_KERNEL_INTERRUPT_PRIORITY	15
 
 #endif /* FREERTOS_CONFIG_H */
-
-
--- 1basic/src/main.c
+++ 1basic/src/main.c
@@ -1,6 +1,29 @@
+#if defined (__AVR_ATmega32U4__)
+#include "FreeRTOSVariant.h"
+#endif
 #include "FreeRTOS.h"
 #include "task.h"
 #include "common.h"
+
+void vLedsFloat(void* dummy);
+void vLedsFlash(void* dummy);
+void vPrintUart(void* dummy);
+
+int main(void){
+ volatile int i;
+ Usart1_Init(); // inits clock as well
+ Led_Init();
+ Led_Hi1();
+
+ if (!(pdPASS == xTaskCreate( vLedsFloat, (signed char*) "LedFloat",64,NULL,1,NULL ))) goto hell;
+ if (!(pdPASS == xTaskCreate( vLedsFlash, (signed char*) "LedFlash",64,NULL,2,NULL ))) goto hell;
+ if (!(pdPASS == xTaskCreate( vPrintUart, (signed char*) "Uart",    64,NULL,3,NULL ))) goto hell;
+
+ vTaskStartScheduler();
+hell:              // should never be reached
+	while(1);
+    return 0;
+}
 
 void vLedsFloat(void* dummy)
 {while(1){
@@ -22,27 +45,10 @@ void vLedsFlash(void* dummy)
 
 /* Writes each 500 ms */
 void vPrintUart(void* dummy)
-{portTickType last_wakeup_time;
+{char c[256];
+ portTickType last_wakeup_time;
  last_wakeup_time = xTaskGetTickCount();
- // while (1) {}
- while(1){uart_puts("Hello World\r\n");
+ while(1){uart_puts("\nHello World\r\n");
 	  vTaskDelayUntil(&last_wakeup_time, 500/portTICK_RATE_MS);
 	}
 }
-
-int main(void){
- volatile int i;
- Usart1_Init(); // inits clock as well
- Led_Init();
- Led_Hi1();
-
- if (!(pdPASS == xTaskCreate( vLedsFloat, (signed char*) "LedFloat",64,NULL,1,NULL ))) goto hell;
- if (!(pdPASS == xTaskCreate( vLedsFlash, (signed char*) "LedFlash",64,NULL,2,NULL ))) goto hell;
- if (!(pdPASS == xTaskCreate( vPrintUart, (signed char*) "Uart",    64,NULL,3,NULL ))) goto hell;
-
- vTaskStartScheduler();
-hell:              // should never be reached
-	while(1);
-    return 0;
-}
-
