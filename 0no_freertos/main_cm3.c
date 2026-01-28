eeb3f5a7b3cbc92621e8eb2a7ece3ad66be21133
Jean-Michel Friedt <friedtj@free.fr>
2026-01-28 18:29:06 +0100

    cleanup 0 and 1

--- 0no_freertos/main_cm3.c
+++ 0no_freertos/main_cm3.c
@@ -1,6 +1,15 @@
 #include "common.h"
-
 #include "../common/avec_newlib.h"
+
+void disp8(char c)
+{char tmp;
+ tmp=(c>>4);  if (tmp<10) uart_putc(tmp + '0'); else uart_putc(tmp + 'A'-10);
+ tmp=(c&0xf); if (tmp<10) uart_putc(tmp + '0'); else uart_putc(tmp + 'A'-10);
+}
+
+void disp16(short s) {disp8(s>>8); disp8(s&0xff); }
+
+void disp32(short s) {disp16(s>>16); disp16(s&0xffff); }
 
 #ifdef avec_newlib
 #include <errno.h>
@@ -17,10 +26,10 @@ int main(void)
     if (c&0x01) {Led_Hi1();Led_Hi2();} else {Led_Lo1();Led_Lo2();}
     c = (c == 9) ? 0 : c + 1;	// cyclic increment c
 #ifndef avec_newlib
-    uart_putc(c + '0'); // USART: send byte
+    disp32(c); // USART: send byte
     uart_puts("\r\n\0");
 #else
-    printf("%d\r\n", (int)c); 
+    printf("%08x\r\n", (int)c); 
 #endif
     for (i = 0; i < 800000; i++) __asm__("NOP");
   }
